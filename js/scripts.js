5602a1021477dba55b0858e6548f0df84585585c
Susan Southall <sue@proteanaegis.com>
2018-04-06 11:56:15 -0700

    Add first BDD condition, add a topping.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-ECHO is on.
+// NOTE: BEgins Business Logic.
+
+
+function Pizza(topping) {
+  this.size = size;
+  this.toppings = "pepporoni";
+}
+
+// Pizza.prototype.cost = function() {
+//   if (this.size === "small") {
+//     this.price = "$10.00";
+//   }else if (this.size === "medium") {
+//     this.price = "$15.00";
+//   }else {
+//     this.price = "$20.00";
+//   }
+// }
