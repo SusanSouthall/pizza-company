f0513c2133a7cb8a9f6f01292f08773d2241a1de
Susan Southall <sue@proteanaegis.com>
2018-04-06 12:15:51 -0700

    Add 3rd BDD, determine price of pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ function Pizza(size, topping) {
   this.toppings = "pepporoni";
 }
 //
-// Pizza.prototype.cost = function() {
-//   if (this.size === "small") {
-//     return this.price = "$10.00";
-//   }else if (this.size === "medium") {
-//     return this.price = "$15.00";
-//   }else {
-//     return this.price = "$20.00";
-//   }
-// }
+Pizza.prototype.cost = function() {
+  if (this.size === "small") {
+    return this.price = "$10.00";
+  }else if (this.size === "medium") {
+    return this.price = "$15.00";
+  }else {
+    return this.price = "$20.00";
+  }
+}
