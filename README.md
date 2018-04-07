9267e3032a03ca2b8c1d0080bab537a559a423b7
Susan Southall <sue@proteanaegis.com>
2018-04-06 17:48:57 -0700

    Fix jquery issue with dropdown menu, add README.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-ECHO is on.
+# Pizza website
+
+#### A website to order a pizza. 4/6/18
+
+#### By **Susan Southall**
+
+## Description
+
+A website that allows a person to choose pizza toppings and size and informs them of the cost of the pizza.
+
+### Specs
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| **A person can choose an individual topping.** | A person clicks on "pepperoni". | Output: "You have ordered a pepporoni pizza." |
+| **A person can choose a size of pizza.**| A person clicks on size: small. | Output: "You have ordered a small pizza." |
+| *Program determines price of pizza based on size.**| A person clicks on "pepperoni" and chooses size: small.  | Output: "You have ordered a small pepperoni pizza." |
+| **Program allows multiply toppings to be added to a pizza.** | A person clicks on "pepperoni" and "mushrooms". | Output: "You have ordered a pepperoni and mushroom pizza." |
+
+## Setup/Installation Requirements
+
+1. In your terminal, cd to the folder you want to save the file.
+2. Use Git clone to save this project locally.
+3. Open index.html in your browser to view this project.
+
+## Known Bugs
+* No known bugs at this time.
+
+## Technologies Used
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+
+### License
+
+*{This software is licensed under the MIT license}*
+
+Copyright (c) 2018 **_Susan Southall_**
--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   }
-  
+
 Pizza.prototype.cost = function() {
   if (this.size === "small") {
     return this.price = "$10.00";
@@ -15,3 +15,57 @@ Pizza.prototype.cost = function() {
     return this.price = "$20.00";
   }
 }
+
+
+
+
+
+// NOTE: Begins User Interface Logic.
+
+$(document).ready(function(){
+
+  $("#welcome").submit(function(event){
+    event.preventDefault();
+
+    var pepperoni = $("#pepperoni").val();
+    var mushrooms = $("#mushrooms").val();
+    var onions = $("#onions").val();
+    var sausage = $("#sausage").val();
+    var bacon = $("#bacon").val();
+    var cheese = $("#cheese").val();
+    var olives = $("#olives").val();
+    var peppers = $("#peppers").val();
+    var tomatoes =$("#tomatoes").val();
+    var pineapple =$("#pineapple").val();
+    var spinach = $("#spinach").val();
+    var pizzaSize = $("#pizzaSize").val();
+    var pizzaToppings = ["pepperoni", "mushrooms", "onions", "sausage", "bacon", "cheese", "olives", "peppers", "tomatoes", "pineapple", "spinach"];
+    var personalPizzaToppings = [];
+    var personalPizza = new Pizza(pizzaSize, personalPizzaToppings);
+    //
+  $("input[type=checkbox]:checked").each(function(topping){
+     personalPizzaToppings.push(topping);
+    console.log(personalPizzaToppings);
+
+  });
+
+    console.log(personalPizzaToppings);
+    // if(document.getElementById("#mushrooms").checked) {
+    //   console.log("mushrooms");
+    // }
+
+    //   }) {
+    //     personalPizza.size = "small";
+    //   }else if ($("#medium").attr("checked")) {
+    //     personalPizza.size = "medium";
+    //   }else {
+    //     personalPizza.size = "large";
+    //   }return personalPizza.size;
+    // }
+
+// var personalPizzaSize = checkSize();
+//     console.log(personalPizzaSize);
+//
+
+  });
+});
